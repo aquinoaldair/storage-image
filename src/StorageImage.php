38c052b8f61ddb706ad1b2e8338696a4b0a7e2e9
Aldair <aquinoaldair@hotmail.com>
2021-04-09 17:05:32 -0500

    Provide easy methods to store

--- src/StorageImage.php
+++ src/StorageImage.php
@@ -3,6 +3,10 @@
 namespace AquinoAldair\StorageImage;
 
 use AquinoAldair\StorageImage\Contract\Image;
+use AquinoAldair\StorageImage\Strategies\FromBase64;
+use AquinoAldair\StorageImage\Strategies\FromFormData;
+use AquinoAldair\StorageImage\Strategies\FromString;
+use AquinoAldair\StorageImage\Strategies\FromURL;
 
 class StorageImage
 {
@@ -12,5 +16,21 @@ class StorageImage
 
     public static function make(Image $image){
         return (new StorageImage)->select($image);
+    }
+
+    public static function FormData(){
+        return self::make(new FromFormData);
+    }
+
+    public static function FromBase64(){
+        return self::make(new FromBase64);
+    }
+
+    public static function FromURL(){
+        return self::make(new FromURL);
+    }
+
+    public static function FromString(){
+        return self::make(new FromString);
     }
 }
